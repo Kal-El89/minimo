200f641837f1a8d26072f29b7d59b1bad0b8176d
Kal-El89 <tufuria89@gmail.com>
2020-02-06 01:56:32 +0400

    burger menu

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let burgerElement = document.getElementById('burger');
+let iconsElement = document.getElementById('icon');
+
+burgerElement.addEventListener('click', function(){
+burger.classList.toggle("active");
+iconsElement.classList.toggle("active");
+})
